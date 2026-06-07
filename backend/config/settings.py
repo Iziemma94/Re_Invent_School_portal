235b11cc25f1bf41e86ededb36ea8f5d2e5e9e79
Iziemma94 <110993682+Iziemma94@users.noreply.github.com>
2026-06-07 16:36:45 +0100

    Fix CORS and production API settings

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -79,11 +79,9 @@ REST_FRAMEWORK = {
 
 CORS_ALLOW_CREDENTIALS = True
 CORS_ALLOWED_ORIGINS = [
-    "https://re-invent-school-frontend.vercel.app",
     "https://re-invent-school-portal.vercel.app",
 ]
 CSRF_TRUSTED_ORIGINS = [
-    "https://re-invent-school-frontend.vercel.app",
     "https://re-invent-school-portal.vercel.app",
 ]
 
